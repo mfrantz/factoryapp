36df4c52edb540d9d9e4d247ffb82dfe606d68fc
Charles Frantz <fhcfrantz@Charless-MacBook-Air.local>
2014-05-27 18:34:32 -0400

    Initial Commit

---
+++ FactoryApp/MFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFAppDelegate.h
+//  FactoryApp
+//
+//  Created by Charles Frantz on 5/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FactoryApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FactoryApp
+//
+//  Created by Charles Frantz on 5/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFAppDelegate class]));
+    }
+}
